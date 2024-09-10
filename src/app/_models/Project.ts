d624b56e6cf2c99cea23d180c8c746e61b602bc3
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-09-10 10:59:11 +0300

    personnalisation de couleur

--- src/app/_models/Project.ts
+++ src/app/_models/Project.ts
@@ -1,8 +1,11 @@
+import { Tag } from "./Tag";
+
 export interface Project{
     id: number;
     name: string;
     summary: string;
     description: string;
-    projectLink: string[];
-    tags: string[];
+    projectLink: string;
+    pictures: string[];
+    tags: Tag[];
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
+import { Tag } from '../_models/Tag';
 
 @Component({
   selector: 'app-portfolio',
@@ -15,7 +16,7 @@ export class PortfolioComponent {
     summary: 'Test Description',
     description: '',
     projectLink: '',
-    tags: ['Angular', 'TypeScript'],
+    tags: [Tag.ANGULAR, Tag.TYPESCRIPT],
     pictures: []
   };
   // Creation de titre de page
