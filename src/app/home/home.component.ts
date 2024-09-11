4f43f82ea62d1eb12b2833eb6ba2c10252f0b401
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-09-11 09:48:26 +0300

    creation de service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { ProjectsService } from './../_services/projects.service';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/Project';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  featuredProject = {} as Project;
   // Creation de titre de page
-  constructor(private titleService: Title){
+  constructor(private titleService: Title, private projectService: ProjectsService){
     this.titleService.setTitle('Rivo Andriamifidy - Home ')
+  }
+  ngOnInit(): void {
+    this.featuredProject = this.projectService.GetProjectById(0);
   }
 
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,25 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
-import { Tag } from '../_models/Tag';
+import { ProjectsService } from '../_services/projects.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit{
 
-  projects: Project[] = [
-    {id: 0, name: "Sample Python Project", pictures: [], projectLink: "//www.github.com", summary: "Python project that analyzes stock market data.", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet facilis corporis exercitationem ex harum! Neque iusto repudiandae ullam eaque dolores pariatur quos quidem magnam, natus, amet excepturi magni quae ex.", tags: [Tag.PYTHON]},
-    {id: 0, name: "Sample Angular App", pictures: [], projectLink: "//www.github.com", summary: "Fullstack web app developed using Angular and Node.JS", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet facilis corporis exercitationem ex harum! Neque iusto repudiandae ullam eaque dolores pariatur quos quidem magnam, natus, amet excepturi magni quae ex.", tags: [Tag.ANGULAR, Tag.TYPESCRIPT, Tag.NODEJS]},
-    {id: 0, name: "Sample .Net App", pictures: [], projectLink: "//www.github.com", summary: "Fullstack web app developed using React and ASP.NET", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet facilis corporis exercitationem ex harum! Neque iusto repudiandae ullam eaque dolores pariatur quos quidem magnam, natus, amet excepturi magni quae ex.", tags: [Tag.REACT, Tag.CSHARP, Tag.ASPNET]},
-    {id: 0, name: "Web API Project", pictures: [], projectLink: "//www.github.com", summary: "Web API Project that was developed for a class project", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet facilis corporis exercitationem ex harum! Neque iusto repudiandae ullam eaque dolores pariatur quos quidem magnam, natus, amet excepturi magni quae ex.", tags: [Tag.CSHARP, Tag.ASPNET]},
-    {id: 0, name: "Chrome Extension", pictures: [], projectLink: "//www.github.com", summary: "Developed a chrome extension that tracks the prices of furniture", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet facilis corporis exercitationem ex harum! Neque iusto repudiandae ullam eaque dolores pariatur quos quidem magnam, natus, amet excepturi magni quae ex.", tags: [Tag.JAVASCRIPT]},
-    {id: 0, name: "Mobile App", pictures: [], projectLink: "//www.github.com", summary: "Mobile app developed in java that tracks the departure and arrival of trains", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eveniet facilis corporis exercitationem ex harum! Neque iusto repudiandae ullam eaque dolores pariatur quos quidem magnam, natus, amet excepturi magni quae ex.", tags: [Tag.JAVA]}
-  ];
+  projects = {} as Project[];
+  
   // Creation de titre de page
-  constructor(private titleService: Title){
+  constructor(private titleService: Title, private projectService: ProjectsService){
     this.titleService.setTitle('Rivo Andriamifidy - Portfolio ')
+  }
+  ngOnInit(): void {
+    this.projects = this.projectService.GetProjects();
   }
 }
