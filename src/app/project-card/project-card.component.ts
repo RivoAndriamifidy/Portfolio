41c6aff961dd80049c88df973abe7b791fc93dfa
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-09-13 11:24:29 +0300

    modal suite

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Project } from '../_models/Project';
 import { BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { ProjectModalComponent } from '../project-modal/project-modal.component';
@@ -6,26 +6,24 @@ import { ProjectModalComponent } from '../project-modal/project-modal.component'
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
-  styleUrl: './project-card.component.css'
+  styleUrls: ['./project-card.component.css']  // Attention: c'est 'styleUrls' (pluriel)
 })
 export class ProjectCardComponent {
 
-  @Input() project = {} as Project; //titre de card
-
+  @Input() project = {} as Project; // Titre de card
   bsModalRef?: BsModalRef;
 
-  constructor(private modalService: BsModalService){
+  constructor(private modalService: BsModalService) { }
 
-  }
-
-  OpenProjectModal(){
-    const modalOptions: ModalOptions ={
-      class: "modal-lg",
+  OpenProjectModal() {
+    const modalOptions: ModalOptions = {
+      class: 'modal-lg',
       initialState: {
         project: this.project
       }
     };
 
-    this.bsModalRef = this.modalService.show(ProjectModalComponent);
+    // Utilise 'modalOptions' pour passer les données
+    this.bsModalRef = this.modalService.show(ProjectModalComponent, modalOptions);
   }
 }
