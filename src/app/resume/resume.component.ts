75040da430688d300f0b6f2ef87dcc153fa90a20
Rivo Andriamifidy <rivoandriamifidy0@gmail.com>
2024-09-20 10:02:28 +0300

    resume

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -9,8 +9,17 @@ import { Title } from '@angular/platform-browser';
 export class ResumeComponent {
 
   // Creation de titre de page
-  constructor(private titleService: Title){
+  constructor(private titleService: Title, private renderer: Renderer2){
     this.titleService.setTitle('Rivo Andriamifidy - Resume ')
+  }
+
+  DownloadFile(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../asset/CV-Rivo.pdf');
+    link.setAttribute('download', 'CV-Rivo');
+    link.click();
+    link.remove();
   }
 
 }
